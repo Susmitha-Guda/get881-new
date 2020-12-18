54814c61c3dac649c17c04aa02e1128b65c5216c
Susmitha-Guda <76148886+Susmitha-Guda@users.noreply.github.com>
2020-12-18 16:46:51 +0530

    Update calc.java

--- DemoApp/src/com/lti/model/calc.java
+++ DemoApp/src/com/lti/model/calc.java
@@ -18,4 +18,9 @@ public void prod(int num1,int num2)
 	System.out.println("mul= "+(num1*num2));
 	
 }
+	public void division(int num1,int num2)
+{
+	System.out.println("division= "+(num1*num2));
+	
+}
 }
