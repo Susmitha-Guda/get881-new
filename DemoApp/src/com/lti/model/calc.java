7e28b4d80bc33ebd4845527d4b52486519fcd7d5
Susmitha-Guda <76148886+Susmitha-Guda@users.noreply.github.com>
2020-12-18 16:32:53 +0530

    Update calc.java

--- DemoApp/src/com/lti/model/calc.java
+++ DemoApp/src/com/lti/model/calc.java
@@ -3,7 +3,7 @@ package com.lti.model;
 public class calc {
 public void add(int num1,int num2)
 {
-	System.out.println("Sum= "+(num1+num2));
+	("Sum= "+(num1+num2));
 }
 
 public void sub(int num1,int num2)
